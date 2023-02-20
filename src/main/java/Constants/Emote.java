3b0dc380d3bafe9794973de71f7c0f8b182bf943
PatrickShawnNelson <PatrickShawnNelson@gmail.com>
2023-02-20 13:40:59 -0500

    Improved Emote declaration and setting up refactoring of emoji feature

--- src/main/java/Constants/Emote.java
+++ src/main/java/Constants/Emote.java
@@ -4,9 +4,9 @@ public final class Emote {
 
 	public static final String EMOTEDESTINATION = "C:/Users/Shoji/OneDrive/Documents/emojis/";
 	
-	public static final String BRUH = "C:/Users/Shoji/OneDrive/Documents/emojis/bruh.png";
+	public static final String BRUH = EMOTEDESTINATION + "/bruh.png";
 	
-	public static final String CHAD = "C:/Users/Shoji/OneDrive/Documents/emojis/chadbot.png";
+	public static final String CHAD = EMOTEDESTINATION + "/chadbot.png";
 	
-	public static final String GAVIN = "C:/Users/Shoji/OneDrive/Documents/emojis/gavin.png";
+	public static final String GAVIN = EMOTEDESTINATION + "/gavin.png";
 }
